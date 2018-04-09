3d38e27f5c029a21760a0f5963f8ece874f8e197
Coding-Libra <347612433@qq.com>
2018-04-09 16:37:56 +0800

    添加了 SB Admin 2 文件

--- src/main/java/oa/entity/Activity.java
+++ src/main/java/oa/entity/Activity.java
@@ -11,7 +11,10 @@ public class Activity {
 	private String location;
 	private Date starttime;
 	private Date endtime;
-	public Activity() {}
+
+	public Activity() {
+	}
+
 	public Activity(int id, int creatorid, Date createtime, String title, String description, String location,
 			Date starttime, Date endtime) {
 		super();
@@ -24,51 +27,67 @@ public class Activity {
 		this.starttime = starttime;
 		this.endtime = endtime;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getCreatorid() {
 		return creatorid;
 	}
+
 	public void setCreatorid(int creatorid) {
 		this.creatorid = creatorid;
 	}
+
 	public Date getCreatetime() {
 		return createtime;
 	}
+
 	public void setCreatetime(Date createtime) {
 		this.createtime = createtime;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	public Date getStarttime() {
 		return starttime;
 	}
+
 	public void setStarttime(Date starttime) {
 		this.starttime = starttime;
 	}
+
 	public Date getEndtime() {
 		return endtime;
 	}
+
 	public void setEndtime(Date endtime) {
 		this.endtime = endtime;
 	}
