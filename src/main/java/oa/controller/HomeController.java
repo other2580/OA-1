b45a63ff732410109d2317709dd3acec679b6456
1375683266 <1375683266@qq.com>
2018-04-09 17:19:36 +0800

    首页可以运行

---
+++ src/main/java/oa/controller/HomeController.java
@@ -0,0 +1,15 @@
+package oa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("head")
+	public String tiles() {
+		return "index";
+	}
+	
+	
+}
