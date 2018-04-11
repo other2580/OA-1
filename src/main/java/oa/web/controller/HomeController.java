65341f079a19a68316a8d1f820298d44c029e39c
1375683266 <1375683266@qq.com>
2018-04-11 16:00:47 +0800

    22

--- src/main/java/oa/web/controller/HomeController.java
+++ src/main/java/oa/web/controller/HomeController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("head")
+	@RequestMapping("index")
 	public String tiles() {
 		return "index";
 	}
-	
+	@RequestMapping("person")
+	public String person() {
+		return "person-alaffairs";
+	}
 	
 }
